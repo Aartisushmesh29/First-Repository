35d083887a8d5679f7dbe23b667de3d70e1a2ad5
Aartisushmesh29 <aartisushmesh@gmail.com>
2023-10-03 13:28:49 +0530

    Organized Files and Folders

--- Projects/WebApp/first.js
+++ Projects/WebApp/first.js
@@ -1,11 +1,28 @@
-console.log("i like pizza");
-window.alert("i really love pizza");
-// variable
+let a;
+let b;
+let c;
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("atextbox").value;
+    a = Number(a);
 
-let firstName = "Bro";  //strings
-let age = 21 ; //number
-let student = true ; //booleans
+    b = document.getElementById("btextbox").value;
+    b = Number(b);
 
-console.log("hello",firstName);
-console.log("you are",age,"years old");
-console.log("enrolled:",student);
+    c = Math.sqrt(Math.pow(a,2) + Math.pow(b, 2));
+
+    document.getElementById("clabel").innerHTML = "Side C:" + c;
+
+}
+// console.log("i like pizza");
+// window.alert("i really love pizza");
+// // variable
+
+// let firstName = "Bro";  //strings
+// let age = 21 ; //number
+// let student = true ; //booleans
+
+// console.log("hello",firstName);
+// console.log("you are",age,"years old");
+// console.log("enrolled:",student);
+
+
